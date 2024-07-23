583395ca6933a44c469d2024f93b4343cb40c5d9
Lucie-Baborova <lucie.babor@gmail.com>
2024-07-23 21:09:55 +0200

    routes setup (angular essentials)

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,8 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './home.component';
+import { ProductsComponent } from './products.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: '', component: HomeComponent },
+    { path: 'products', component: ProductsComponent }
+];
