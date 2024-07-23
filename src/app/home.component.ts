1716922aff7a3366296a122f4bd0f213fbd0e6c7
Lucie-Baborova <lucie.babor@gmail.com>
2024-07-23 21:08:50 +0200

    home component setup (angular essentials)

---
+++ src/app/home.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core'
+
+
+@Component( {
+    selector: 'app-home',
+    template: '<h1>The Home Component</h1>',
+    standalone: true
+})
+
+export class HomeComponent {}
