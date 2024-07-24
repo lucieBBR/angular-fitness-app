0a7b072d7dfca2bf90cd9fe95573dcc5f08e24fa
Lucie-Baborova <lucie.babor@gmail.com>
2024-07-24 12:33:34 +0200

    routes setup

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,8 +1,16 @@
 import { Routes } from '@angular/router';
-import { HomeComponent } from './home.component';
-import { ProductsComponent } from './products.component';
+import { WelcomeComponent } from './welcome/welcome.component';
+import { SignupComponent } from './auth/signup/signup.component';
+import { LoginComponent } from './auth/login/login.component';
+import { TrainingComponent } from './training/training.component';
+//import { HomeComponent } from './home.component';
+//import { ProductsComponent } from './products.component';
 
 export const routes: Routes = [
-    { path: '', component: HomeComponent },
-    { path: 'products', component: ProductsComponent }
+  //  { path: '', component: HomeComponent },
+  //  { path: 'products', component: ProductsComponent }
+  { path: '', component: WelcomeComponent },
+  { path: 'signup', component: SignupComponent },
+  { path: 'login', component: LoginComponent },
+  { path: 'training', component: TrainingComponent }
 ];
