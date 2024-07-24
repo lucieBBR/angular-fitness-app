363d946c47747c318e461a7873f5f5e8cb2f7cea
Lucie-Baborova <lucie.babor@gmail.com>
2024-07-24 12:33:06 +0200

    new app components

---
+++ src/app/auth/signup/signup.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { MaterialModule } from '../../material.module';
+
+@Component({
+  selector: 'app-signup',
+  standalone: true,
+  imports: [MaterialModule],
+  templateUrl: './signup.component.html',
+  styleUrl: './signup.component.css'
+})
+export class SignupComponent {
+
+}
---
+++ src/app/training/past-trainings/past-trainings.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-past-trainings',
+  standalone: true,
+  imports: [],
+  templateUrl: './past-trainings.component.html',
+  styleUrl: './past-trainings.component.css'
+})
+export class PastTrainingsComponent {
+
+}
