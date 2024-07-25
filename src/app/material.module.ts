3a5f5dc84672995153afa4280855c0e58cbcf8cc
Lucie-Baborova <lucie.babor@gmail.com>
2024-07-25 18:47:06 +0200

    imports updated

--- src/app/material.module.ts
+++ src/app/material.module.ts
@@ -9,6 +9,7 @@ import { MatNativeDateModule } from '@angular/material/core';
 import { MatCheckboxModule } from '@angular/material/checkbox';
 import { MatSidenavModule } from '@angular/material/sidenav';
 import { MatToolbarModule } from '@angular/material/toolbar';
+import {MatListModule} from '@angular/material/list';
 
 @NgModule({
     imports: [
@@ -21,7 +22,8 @@ import { MatToolbarModule } from '@angular/material/toolbar';
         MatNativeDateModule,
         MatCheckboxModule,
         MatSidenavModule,
-        MatToolbarModule],
+        MatToolbarModule,
+        MatListModule],
     exports: [
         FlexLayoutModule, 
         MatButtonModule, 
@@ -32,7 +34,8 @@ import { MatToolbarModule } from '@angular/material/toolbar';
         MatNativeDateModule,
         MatCheckboxModule,
         MatSidenavModule,
-        MatToolbarModule], 
+        MatToolbarModule,
+        MatListModule], 
 })
 
 export class MaterialModule { }
