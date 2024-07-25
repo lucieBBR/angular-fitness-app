48ddd6a74768140199bf73a501be42d047a9f443
Lucie-Baborova <lucie.babor@gmail.com>
2024-07-25 17:41:28 +0200

    sign up form (template-driven)

--- src/app/auth/signup/signup.component.ts
+++ src/app/auth/signup/signup.component.ts
@@ -1,13 +1,23 @@
 import { Component } from '@angular/core';
 import { MaterialModule } from '../../material.module';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-signup',
   standalone: true,
-  imports: [MaterialModule],
+  imports: [MaterialModule, FormsModule],
   templateUrl: './signup.component.html',
   styleUrl: './signup.component.css'
 })
 export class SignupComponent {
+  maxDate;
 
+  ngOnInit() {
+    this.maxDate = new Date();
+    this.maxDate.setFullYear(this.maxDate.getFullYear() - 18);
+  }
+
+  onSubmit(form: FormsModule) {
+    console.log(form);
+  }
 }
