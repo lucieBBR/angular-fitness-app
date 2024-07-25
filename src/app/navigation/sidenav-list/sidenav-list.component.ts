5f60b6b9f385c38a31236f493895f5167435652b
Lucie-Baborova <lucie.babor@gmail.com>
2024-07-25 18:51:13 +0200

    new components for navigation

---
+++ src/app/navigation/sidenav-list/sidenav-list.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-sidenav-list',
+  standalone: true,
+  imports: [],
+  templateUrl: './sidenav-list.component.html',
+  styleUrl: './sidenav-list.component.css'
+})
+export class SidenavListComponent {
+
+}
