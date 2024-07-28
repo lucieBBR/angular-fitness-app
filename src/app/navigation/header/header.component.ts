1e6e1b72c4a023fb3c73a4fddc107c6c35fa06f9
Lucie-Baborova <lucie.babor@gmail.com>
2024-07-28 16:44:29 +0200

    new header component (nav splitting)

--- src/app/navigation/header/header.component.ts
+++ src/app/navigation/header/header.component.ts
@@ -1,12 +1,18 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, EventEmitter, Output } from '@angular/core';
+import { MaterialModule } from '../../material.module';
 
 @Component({
   selector: 'app-header',
   standalone: true,
-  imports: [],
+  imports: [MaterialModule],
   templateUrl: './header.component.html',
   styleUrl: './header.component.css'
 })
 export class HeaderComponent {
 
+  @Output() sidenavToggle = new EventEmitter<void>()
+
+  onToggleSidenav() {
+    this.sidenavToggle.emit();
+  }
 }
