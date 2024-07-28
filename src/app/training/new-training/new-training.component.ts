da85ce09dcd0f0e5432a65bd81df88c3a3c959cd
Lucie-Baborova <lucie.babor@gmail.com>
2024-07-28 16:47:07 +0200

    new training creation

--- src/app/training/new-training/new-training.component.ts
+++ src/app/training/new-training/new-training.component.ts
@@ -1,12 +1,17 @@
-import { Component } from '@angular/core';
+import { Component, EventEmitter, Output } from '@angular/core';
+import { MaterialModule } from '../../material.module';
 
 @Component({
   selector: 'app-new-training',
   standalone: true,
-  imports: [],
+  imports: [MaterialModule],
   templateUrl: './new-training.component.html',
   styleUrl: './new-training.component.css'
 })
 export class NewTrainingComponent {
+  @Output() trainingStart = new EventEmitter<void>();
 
+  onStartTraining() {
+    this.trainingStart.emit();
+  }
 }
