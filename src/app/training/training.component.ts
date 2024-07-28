94fbc2b284ff32535726c712743779099cbf1aac
Lucie-Baborova <lucie.babor@gmail.com>
2024-07-28 16:46:10 +0200

    training component with tabs

--- src/app/training/training.component.ts
+++ src/app/training/training.component.ts
@@ -1,12 +1,16 @@
 import { Component } from '@angular/core';
+import { MaterialModule } from '../material.module';
+import { NewTrainingComponent } from './new-training/new-training.component';
+import { PastTrainingsComponent } from './past-trainings/past-trainings.component';
+import { CurrentTrainingComponent } from "./current-training/current-training.component";
 
 @Component({
   selector: 'app-training',
   standalone: true,
-  imports: [],
+  imports: [MaterialModule, NewTrainingComponent, PastTrainingsComponent, CurrentTrainingComponent],
   templateUrl: './training.component.html',
   styleUrl: './training.component.css'
 })
 export class TrainingComponent {
-
+  ongoingTraining = false;
 }
