7759084d121bc18b9e69887fd067000abcc18d2b
Lucie-Baborova <lucie.babor@gmail.com>
2024-07-28 16:43:13 +0200

    splitting navigation into separate components

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,13 @@
 import { Component, ViewChild } from '@angular/core';
 import { RouterModule } from '@angular/router';
 import { MaterialModule } from './material.module';
+import { HeaderComponent } from "./navigation/header/header.component";
+import { SidenavListComponent } from "./navigation/sidenav-list/sidenav-list.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterModule, MaterialModule],
+  imports: [RouterModule, MaterialModule, HeaderComponent, SidenavListComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
 })
