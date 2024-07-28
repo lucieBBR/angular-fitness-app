a15d0fcba5b6ce1b1ba99213341ce708b421a234
Lucie-Baborova <lucie.babor@gmail.com>
2024-07-28 16:44:51 +0200

    new sidenav component (nav splitting)

--- src/app/navigation/sidenav-list/sidenav-list.component.ts
+++ src/app/navigation/sidenav-list/sidenav-list.component.ts
@@ -1,12 +1,18 @@
-import { Component } from '@angular/core';
+import { Component, EventEmitter, Output } from '@angular/core';
+import { MaterialModule } from '../../material.module';
 
 @Component({
   selector: 'app-sidenav-list',
   standalone: true,
-  imports: [],
+  imports: [MaterialModule],
   templateUrl: './sidenav-list.component.html',
   styleUrl: './sidenav-list.component.css'
 })
 export class SidenavListComponent {
 
+  @Output() closeSidenav = new EventEmitter<void>();
+
+  onClose(){
+    this.closeSidenav.emit();
+  }
 }
