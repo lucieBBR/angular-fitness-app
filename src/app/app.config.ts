937a07da99988af3eae9882b28a8da970abd6127
Lucie-Baborova <lucie.babor@gmail.com>
2024-07-28 17:40:19 +0200

    adding AuthService to providers

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -2,9 +2,11 @@ import { ApplicationConfig } from '@angular/core';
 import { provideRouter } from '@angular/router';
 import { routes } from './app.routes';
 import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
+import { AuthService } from './auth/auth.service';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideRouter(routes),
-    provideAnimationsAsync()]
+    provideAnimationsAsync(),
+    AuthService]
 };
