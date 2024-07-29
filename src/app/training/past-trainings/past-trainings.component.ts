440eaa83b20a747043d35e61737af364c4aa6862
Lucie-Baborova <lucie.babor@gmail.com>
2024-07-29 10:56:09 +0200

    displaying past training in a table

--- src/app/training/past-trainings/past-trainings.component.ts
+++ src/app/training/past-trainings/past-trainings.component.ts
@@ -1,13 +1,24 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { CommonModule } from '@angular/common';
 import { MaterialModule } from '../../material.module';
+import { MatTableDataSource } from '@angular/material/table';
+import { Exercise } from '../exercise.model';
+import { TrainingService } from '../training.service';
 
 @Component({
   selector: 'app-past-trainings',
   standalone: true,
-  imports: [MaterialModule],
+  imports: [CommonModule, MaterialModule],
   templateUrl: './past-trainings.component.html',
   styleUrl: './past-trainings.component.css'
 })
-export class PastTrainingsComponent {
+export class PastTrainingsComponent implements OnInit {
+  displayedColumns = ['date', 'name', 'duration', 'calories', 'state' ];
+  dataSource = new MatTableDataSource<Exercise>();
 
+  constructor(private trainingService: TrainingService) { }
+
+  ngOnInit() {
+    this.dataSource.data = this.trainingService.getCompletedOrCancelledExercises();
+  }
 }
