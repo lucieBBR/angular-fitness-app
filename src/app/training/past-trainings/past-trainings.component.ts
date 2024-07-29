686e1d1f1add04fe2b17dc32d35ac5e00969696b
Lucie-Baborova <lucie.babor@gmail.com>
2024-07-29 11:34:20 +0200

    table filtering added

--- src/app/training/past-trainings/past-trainings.component.ts
+++ src/app/training/past-trainings/past-trainings.component.ts
@@ -28,4 +28,8 @@ export class PastTrainingsComponent implements OnInit, AfterViewInit {
   ngAfterViewInit() {
     this.dataSource.sort = this.sort;
   }
+
+  doFilter(filterValue: string) {
+    this.dataSource.filter = filterValue.trim().toLowerCase();
+  }
 }
