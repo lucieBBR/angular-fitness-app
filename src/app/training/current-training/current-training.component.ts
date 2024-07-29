701daed251471ac3f0d64fb36bc92b4f7288c278
Lucie-Baborova <lucie.babor@gmail.com>
2024-07-29 10:54:11 +0200

    cancel or complete exercise using TrainingService

--- src/app/training/current-training/current-training.component.ts
+++ src/app/training/current-training/current-training.component.ts
@@ -1,7 +1,8 @@
-import { Component, EventEmitter, Output } from '@angular/core';
+import { Component } from '@angular/core';
 import { MaterialModule } from '../../material.module';
 import { MatDialog } from '@angular/material/dialog';
 import { StopTrainingComponent } from './stop-training.component';
+import { TrainingService } from '../training.service';
 
 @Component({
   selector: 'app-current-training',
@@ -11,23 +12,24 @@ import { StopTrainingComponent } from './stop-training.component';
   styleUrl: './current-training.component.css',
 })
 export class CurrentTrainingComponent {
-  @Output() trainingExit = new EventEmitter();
   progress = 0;
   timer: ReturnType<typeof setInterval>;
 
-  constructor(private dialog: MatDialog) { }
+  constructor(private dialog: MatDialog, private trainingService: TrainingService) { }
 
   ngOnInit() {
     this.startOrResumerTimer();
   }
 
   startOrResumerTimer() {
+    const step = this.trainingService.getRunningExercise().duration / 100 * 1000;
     this.timer = setInterval(() => {
-    this.progress = this.progress + 5;
+    this.progress = this.progress + 1;
     if (this.progress >= 100) {
+      this.trainingService.completeExercise();
       clearInterval(this.timer);
     }
-  }, 1000);
+  }, step);
   }
 
   onStop() {
@@ -40,7 +42,7 @@ export class CurrentTrainingComponent {
 
   dialogRef.afterClosed().subscribe(result => {
     if (result) {
-      this.trainingExit.emit();
+      this.trainingService.cancelExercise(this.progress);
     } else {
        this.startOrResumerTimer();
     }
