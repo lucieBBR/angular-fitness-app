13f0bd79b53d56dbe5a8f302b9e714119a5083b2
Lucie-Baborova <lucie.babor@gmail.com>
2024-07-29 10:51:30 +0200

    new providers

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -3,10 +3,14 @@ import { provideRouter } from '@angular/router';
 import { routes } from './app.routes';
 import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
 import { AuthService } from './auth/auth.service';
+import { AuthGuard } from './auth/auth.guard';
+import { TrainingService } from './training/training.service';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideRouter(routes),
     provideAnimationsAsync(),
-    AuthService]
+    AuthService,
+    AuthGuard,
+    TrainingService]
 };
