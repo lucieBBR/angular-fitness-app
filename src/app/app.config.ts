ee4f1abbc10e51942621defd428fc7f3e26f8230
Lucie-Baborova <lucie.babor@gmail.com>
2024-07-30 13:40:09 +0200

    Firebase setup (with angular fire)

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -5,11 +5,16 @@ import { provideAnimationsAsync } from '@angular/platform-browser/animations/asy
 import { AuthService } from './auth/auth.service';
 import { AuthGuard } from './auth/auth.guard';
 import { TrainingService } from './training/training.service';
+import { provideFirebaseApp, initializeApp } from '@angular/fire/app';
+import { getFirestore, provideFirestore } from '@angular/fire/firestore';
+import { environment } from 'src/environments/environment';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideRouter(routes),
     provideAnimationsAsync(),
+    provideFirebaseApp(() => initializeApp(environment.firebase)),
+    provideFirestore(() => getFirestore()),
     AuthService,
     AuthGuard,
     TrainingService]
