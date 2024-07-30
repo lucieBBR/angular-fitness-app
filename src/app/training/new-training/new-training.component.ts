14a362bff91e8affe0bbbccff54e92a1e9d44934
Lucie-Baborova <lucie.babor@gmail.com>
2024-07-30 13:40:36 +0200

    displaying data from firebase

--- src/app/training/new-training/new-training.component.ts
+++ src/app/training/new-training/new-training.component.ts
@@ -1,23 +1,49 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, inject } from '@angular/core';
 import { MaterialModule } from '../../material.module';
 import { TrainingService } from '../training.service';
 import { Exercise } from '../exercise.model';
 import { FormsModule, NgForm } from '@angular/forms';
+import { Firestore, collectionSnapshots, collection } from '@angular/fire/firestore';
+import { Observable } from 'rxjs';
+import { map } from 'rxjs/operators';
+// import 'rxjs/add/operator/map'
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-new-training',
   standalone: true,
-  imports: [MaterialModule, FormsModule],
+  imports: [CommonModule, MaterialModule, FormsModule],
   templateUrl: './new-training.component.html',
   styleUrl: './new-training.component.css'
 })
 export class NewTrainingComponent implements OnInit {
-  exercises: Exercise[] = [];
+  exercises!: Observable<Exercise[]>;
+  firestore: Firestore = inject(Firestore);
+  
 
-  constructor(private trainingService: TrainingService) { }
+  constructor(private trainingService: TrainingService) {
 
-  ngOnInit() {
-    this.exercises = this.trainingService.getAvailableExercises();
+  }
+
+  ngOnInit(): void {
+    //this.exercises = this.trainingService.getAvailableExercises();
+   // console.log(this.exercises)
+    const exerciseCollection = collection(this.firestore, 'availableExercises')
+    // Using snapshotChanges to get document data along with metadata
+    this.exercises = collectionSnapshots(exerciseCollection).pipe(
+      map(docArray => {
+        return docArray.map(doc => {
+          const data = doc.data() as Exercise;
+          const id = doc.id;
+          return { id, ...data };
+        });
+      })
+    ) as Observable<Exercise[]>;
+
+
+    this.exercises.subscribe(result => {
+      console.log(result);
+    });
   }
 
   onStartTraining(form: NgForm) {
